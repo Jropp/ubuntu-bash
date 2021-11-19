c5e270f5830856439ec7a0f69ba4da2ab577a816
Jason Ropp <jasonropp4tl@gmail.com>
2021-11-19 13:11:21 -0500

    Use moar env variables

--- main.sh
+++ main.sh
@@ -4,24 +4,24 @@ source ~/ubuntu-bash/nginx.sh
 alias src='cd; source .bashrc'
 
 alias fdapp="cd && cd $APP_FOLDER_PATH"
-alias fdapi='cd && cd elefnt-crypto-api'
-alias fddocs='cd && cd Staging-Docs'
+alias fdapi="cd && cd $API_FOLDER_PATH"
+alias fdscripts="cd && cd $SCRIPTS_FOLDER_PATH"
 
-alias pullapp='fdapp && ssh-agent bash -c "ssh-add ~/.ssh/app_rsa; git pull"'
-alias pullapi='fdapi && ssh-agent bash -c "ssh-add ~/.ssh/api_rsa; git pull"'
-alias pulldocs='fddocs && ssh-agent bash -c "ssh-add ~/.ssh/docs_rsa; git pull"'
+alias pullapp="fdapp && ssh-agent bash -c \"ssh-add $APP_SSH_PATH; git pull\""
+alias pullapi="fdapi && ssh-agent bash -c \"ssh-add $API_SSH_PATH; git pull]\""
+alias pullscripts='fdscripts && git pull'
 
-buildProd() {
+appRebuild() {
         fdapp
         npm i
         npm run build:prod
-        sudo rm -f /var/www/html/*
-        sudo cp ~/fintech-freedom/dist/* /var/www/html/
+        sudo rm -f $NGINX_APP_FILES_PATH/*
+        sudo cp $APP_FOLDER_PATH/dist/* $NGINX_APP_FILES_PATH
 }
 
-deploy() {
+appDeploy() {
         fdapp
         git reset --hard HEAD
-        ssh-agent bash -c "ssh-add ~/.ssh/app_rsa; git pull"
+        pullapp
         buildProd
 }
